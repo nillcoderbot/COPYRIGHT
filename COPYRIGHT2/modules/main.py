f44bbd84b6d72ed926b1729f79bfa151ed5738e2
YASHU <130467694+nillcoderbot@users.noreply.github.com>
2024-04-16 13:57:07 +0530

    main.py

--- COPYRIGHT2/modules/main.py
+++ COPYRIGHT2/modules/main.py
@@ -40,7 +40,7 @@ async def start(_, msg):
     reply_markup = InlineKeyboardMarkup(buttons)
     
     await msg.reply_photo(
-        photo="https://graph.org/file/7f8ebddf56559ac69d31b.jpg",
+        photo="https://graph.org/file/ca560969be91f1c1fe684.jpg",
         caption=start_txt,
         reply_markup=reply_markup
     )
@@ -49,7 +49,7 @@ async def start(_, msg):
 gd_buttons = [              
         [
             InlineKeyboardButton("ᴏᴡɴᴇʀ", user_id=OWNER_ID),
-            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/HEROKUFREECC"),    
+            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/LINK_KI_DUNIYA"),    
         ]
         ]
 
