9bcac92f75850cda85fe3d0d3b420c0afbcbfb42
YASHU <130467694+nillcoderbot@users.noreply.github.com>
2024-04-16 14:06:58 +0530

    config.py

--- config.py
+++ config.py
@@ -4,18 +4,18 @@
 import os
 from os import getenv
 # ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID"))
+API_ID = int(os.environ.get("9303922"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH")
+API_HASH = os.environ.get("a17677495aa9ae010b897c2d65146282")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("7116315464:AAFS-xZhIgSgw4S8KaFdILv9MyisTDsOz9M")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
+BOT_USERNAME = os.environ.get("COPYRIGHT_CAPTURE_BOT")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID"))
+OWNER_ID = int(os.environ.get("5151455635"))
 # ------------------X------------------------------
 # ------------------------------------------------
-LOGGER_ID = int(os.environ.get("LOGGER_ID"))
+LOGGER_ID = int(os.environ.get("-1001714843083"))
 # ------------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL")
 # ------------------------------------------------
