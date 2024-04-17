1627d7e16a1ffb41cdac01c59d69e87e4794ed81
YASHU <130467694+nillcoderbot@users.noreply.github.com>
2024-04-17 13:29:05 +0530

    config.py

--- config.py
+++ config.py
@@ -4,18 +4,18 @@
 import os
 from os import getenv
 # ---------------R---------------------------------
-API_ID = int(os.environ.get("9303922"))
+API_ID = int(os.environ.get("API_ID"))
 # ------------------------------------------------
-API_HASH = os.environ.get("a17677495aa9ae010b897c2d65146282")
+API_HASH = os.environ.get("API_HASH")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("7116315464:AAFS-xZhIgSgw4S8KaFdILv9MyisTDsOz9M")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("COPYRIGHT_CAPTURE_BOT")
+BOT_USERNAME = os.environ.get("BOT_USERNAME")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("5151455635"))
+OWNER_ID = int(os.environ.get("OWNER_ID"))
 # ------------------X------------------------------
 # ------------------------------------------------
-LOGGER_ID = int(os.environ.get("-1001714843083"))
+LOGGER_ID = int(os.environ.get("LOGGER_ID"))
 # ------------------------------------------------
-MONGO_URL = os.environ.get("mongodb+srv://yashu:yashu@cluster0.he971qc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URL = os.environ.get("MONGO_URL")
 # ------------------------------------------------
